35cac0a0f9c528c774e647bdc7483198fddfddc1
Donghyeon Shin <shindong0321@gmail.com>
2024-12-12 22:54:06 +0900

    Using streamlit Input widget

--- home.py
+++ home.py
@@ -1,8 +1,27 @@
 import streamlit as st
-from langchain.prompts import ChatPromptTemplate
+from datetime import datetime
 
-st.write("hello")
-st.write([1, 2, 3])
-st.write({"x": 1})
+today = datetime.today().strftime("%H:%M:%S")
 
-ChatPromptTemplate
+st.title(today)
+
+model = st.selectbox(
+    "Choose your model",
+    ("GPT-3", "GPT-4"),
+)
+
+if model == "GPT-3":
+    st.write("cheep")
+else:
+    st.write("not cheep")
+
+name = st.text_input("What is your name?")
+st.write(name)
+
+value = st.slider(
+    "htemperature",
+    min_value=0.1,
+    max_value=1.0,
+)
+
+st.write(value)
