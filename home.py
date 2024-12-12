32036e7cb6b2627fd4bdb5243e2b5ffc13974878
Donghyeon Shin <shindong0321@gmail.com>
2024-12-12 23:06:08 +0900

    Make Sidebar and GPT pages

--- home.py
+++ home.py
@@ -1,27 +1,8 @@
 import streamlit as st
-from datetime import datetime
 
-today = datetime.today().strftime("%H:%M:%S")
-
-st.title(today)
-
-model = st.selectbox(
-    "Choose your model",
-    ("GPT-3", "GPT-4"),
+st.set_page_config(
+    page_title="FullstackGPT",
+    page_icon="🤣",
 )
 
-if model == "GPT-3":
-    st.write("cheep")
-else:
-    st.write("not cheep")
-
-name = st.text_input("What is your name?")
-st.write(name)
-
-value = st.slider(
-    "htemperature",
-    min_value=0.1,
-    max_value=1.0,
-)
-
-st.write(value)
+st.title("FullstackGPT Home")
