778f7d1b6d2ba0f2f7a4e167ebc944ffda4c3bde
Donghyeon Shin <shindong0321@gmail.com>
2024-12-13 21:07:15 +0900

    Implement CallbbackHandler to stream chat message

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -1,56 +1,83 @@
 import streamlit as st
-from langchain.storage import LocalFileStore
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
-from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.vectorstores import Chroma
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.callbacks.base import BaseCallbackHandler
 
 st.set_page_config(
     page_title="Document GPT",
     page_icon="🤣",
 )
 
-llm = ChatOpenAI(temperature=0.1)
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ],
+)
 
 
 @st.cache_resource(show_spinner="Embedding file...")
 def embed_file(file):
+    file_name = file.name
     file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
+    file_path = f"./.cache/files/{file_name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=50,
-    )
+    cache_dir = LocalFileStore(f"./cache/embeddings/{file_name}")
     loader = UnstructuredFileLoader(file_path)
-    docs = loader.load_and_split(text_splitter=splitter)
-    embedder = OpenAIEmbeddings()
-    cached_embedding = CacheBackedEmbeddings.from_bytes_store(embedder, cache_dir)
-    vectorStore = Chroma.from_documents(docs, cached_embedding)
-    retriver = vectorStore.as_retriever()
-    return retriver
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n\n",
+        chunk_size=500,
+        chunk_overlap=60,
+    )
+    documents = loader.load_and_split(text_splitter=splitter)
+    embeder = OpenAIEmbeddings()
+    cache_embedder = CacheBackedEmbeddings.from_bytes_store(embeder, cache_dir)
+    vectorStore = Chroma.from_documents(documents, cache_embedder)
+    retriever = vectorStore.as_retriever()
+    return retriever
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
 
 
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
         if save:
-            st.session_state["messages"].append({"message": message, "role": role})
+            save_message(message, role)
 
 
 def paint_history():
-    for message in st.session_state["messages"]:
-        send_message(message["message"], message["role"], save=False)
+    for message_dic in st.session_state["messages"]:
+        send_message(message_dic["message"], message_dic["role"], save=False)
 
 
-def docs_format(docs):
-    return ".\n.\n".join(doc.page_content for doc in docs)
+def format_doc(documents):
+    return "\n\n".join(doc.page_content for doc in documents)
 
 
 prompt = ChatPromptTemplate.from_messages(
@@ -58,10 +85,10 @@ prompt = ChatPromptTemplate.from_messages(
         (
             "system",
             """
-            You are a helpful assistant. Answer questions using only the following context. 
+            You are a helpful assistant. Answer questions using only the following context.
             If you don't know the answer just say you don't know, dont't makt it up:
-            ----------
-            {context},
+            -----
+            {context}
             """,
         ),
         ("human", "{question}"),
@@ -72,11 +99,10 @@ st.title("Document GPT")
 
 st.markdown(
     """
-Welcome!
-            
-Use this chatbot to ask questions to an AI about your files!
-Upload your files on the sidebar.
-"""
+    Welcome!
+    Use this chatbot to ask questions to an AI about your files!
+    Upload your files on the sidebar.
+    """
 )
 
 with st.sidebar:
@@ -87,20 +113,20 @@ with st.sidebar:
 
 if file:
     retriever = embed_file(file)
-    send_message("Hi Can I help you?", "ai", save=False)
+    send_message("How can I help you?", "ai", save=False)
     paint_history()
     question = st.chat_input("Ask anything about your file....")
     if question:
         send_message(question, "human")
         chain = (
             {
-                "context": retriever | RunnableLambda(docs_format),
+                "context": retriever | RunnableLambda(format_doc),
                 "question": RunnablePassthrough(),
             }
             | prompt
             | llm
         )
-        response = chain.invoke(question).content
-        send_message(response, "ai")
+        with st.chat_message("ai"):
+            response = chain.invoke(question)
 else:
     st.session_state["messages"] = []
