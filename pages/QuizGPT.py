5a269c345c0a9822274c3f0208bf77456b6d6147
Donghyeon Shin <shindong0321@gmail.com>
2024-12-16 02:11:03 +0900

    Implement json formatting chain

--- pages/QuizGPT.py
+++ pages/QuizGPT.py
@@ -22,6 +22,173 @@ llm = ChatOpenAI(
 )
 
 
+def format_docs(documents):
+    return "\n\n".join(doc.page_content for doc in documents)
+
+
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a helpful assistant that is role playing as a teacher.
+
+            Based ONLY on the following context make 10 questoins to test the user's knowledge about the text.
+
+            Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+            Use (o) to signal the correct answer.
+
+            Questoin examples
+
+            Question: What is the color of the occean?
+            Answers: Red|Yellow|Green|Blue(o)
+
+            Question: What is the capital or Georgia?
+            Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+            Question: When was Avator released?
+            Answers: 2007|2001|2009(o)|1998
+
+            Question: Who was Julius Caesar?
+            Answers: A Roman Emperor(o)|Painter|Actor|Model
+            
+            Your turn!
+
+            Context: {context}
+            """,
+        )
+    ]
+)
+
+questions_chain = {"context": RunnableLambda(format_docs)} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a powerful formatting algorithm.
+
+            You format exam question intop JSON format.
+            Answers with (o) are the correct ones.
+
+            Example Input:
+
+            Question: What is the color of the occean?
+            Answers: Red|Yellow|Green|Blue(o)
+
+            Question: What is the capital or Georgia?
+            Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+            Question: When was Avator released?
+            Answers: 2007|2001|2009(o)|1998
+
+            Question: Who was Julius Caesar?
+            Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+            Example Output:
+            
+            ```json
+            {{ "question": [
+                    {{
+                        "question": "What is the color of the occean?",
+                        "answers": [
+                            {{
+                                "answer": "Red",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Yellow"
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Green",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Blue",
+                                "correct": true
+                            }},
+                        ]
+                    }},
+                    {{
+                        "question": "What is the capital or Georgia?",
+                        "answers": [
+                            {{
+                                "answer": "Baku",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Tbilisi"
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "Manila",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Beirut",
+                                "correct": false
+                            }},
+                        ]
+                    }},
+                    {{
+                        "question": "When was Avator released?",
+                        "answers": [
+                            {{
+                                "answer": "2007",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "2001"
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "2009",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "1998",
+                                "correct": false
+                            }},
+                        ]
+                    }},
+                    {{
+                        "question": "Who was Julius Caesar?",
+                        "answers": [
+                            {{
+                                "answer": "A Roman Emperor",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "Painter"
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Actor",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Model",
+                                "correct": false
+                            }},
+                        ]
+                    }}                                                
+                ]
+            }}
+
+            Your turn!
+
+            Question : {context}
+            """,
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
+
+
 @st.cache_resource(show_spinner="Loading file...")
 def split_file(file):
     file_name = file.name
@@ -37,9 +204,6 @@ def split_file(file):
     )
     documents = loader.load_and_split(text_splitter=splitter)
     return documents
-
-def format_docs(documents):
-    return "\n\n".join(doc.page_content for doc in documents)
 
 
 with st.sidebar:
@@ -75,41 +239,12 @@ if not docs:
         """
     )
 else:
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
-                You are a helpful assistant that is role playing as a teacher.
-
-                Based ONLY on the following context make 10 questoins to test the user's knowledge about the text.
-
-                Each question should have 4 answers, three of them must be incorrect and one should be correct.
-
-                Use (o) to signal the correct answer.
-
-                Questoin examples
-
-                Question: What is the color of the occean?
-                Answers: Red/Yellow/Green/Blue(o)
-
-                Question: What is the capital or Georgia?
-                Answers: Baku/Tbilisi(o)/Manila/Beirut
-
-                Question: When was Avator released?
-                Answers: 2007/2001/2009(o)/1998
-
-                Question: Who was Julius Caesar?
-                Answers: A Roman Emperor(o)/Painter/Actor/Model
-
-                Your turn!
-
-                Context: {context}
-                """,
-            )
-        ]
-    )
 
     start = st.button("Generate Quiz")
     if start:
-        chain.invoke(docs)
+        question_response = questions_chain.invoke(docs)
+        formatting_response = formatting_chain.invoke(
+            {"context": question_response.content}
+        )
+        st.write(question_response.content)
+        st.write(formatting_response.content)
