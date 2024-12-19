3f14f4732fb76f9b30cf0c36eb61f3697aa8c3c6
Donghyeon Shin <shindong0321@gmail.com>
2024-12-19 21:53:22 +0900

    Implement Map-rerank (2/2)

--- pages/SiteGPT.py
+++ pages/SiteGPT.py
@@ -42,16 +42,54 @@ answers_prompt = ChatPromptTemplate.from_template(
     """
 )
 
+choose_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use ONLY the following pre-existing answers to the user's question.
+
+            Use the answers that have the highest score (more helpful) and favor the most recent ones.
+
+            Return the sources of the answers as they are, do not change them.
+
+            Answer: {answers}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+
 def get_answers(inputs):
     docs = inputs["docs"]
     question = inputs["question"]
     answers_chain = answers_prompt | llm
-    answers = []
-    for doc in docs:
-        result = answers_chain.invoke({"context" : doc.page_content, "question" : question,})
-        answers.append(result.content)
-    st.write(answers)
-    return answers
+    return {
+        "question": question,
+        "answers": [
+            {
+                "answer": answers_chain.invoke(
+                    {
+                        "context": doc.page_content,
+                        "question": question,
+                    }
+                ).content,
+                "source": doc.metadata["source"],
+                "date": doc.metadata["lastmod"],
+            }
+            for doc in docs
+        ],
+    }
+
+
+def choose_answer(inputs):
+    answers = inputs["answers"]
+    question = inputs["question"]
+    choose_chain = choose_prompt | llm
+    condensed = "\n\n".join(f"{answer['answer']}\nSource:{answer['source']}\nDate:{answer['date']}\n" for answer in answers)
+    return choose_chain.invoke({"question": question, "answers": condensed})
+
 
 def parse_page(soup):
     header = soup.find("header")
@@ -114,10 +152,16 @@ if url:
             st.error("Please write down a Stiemap URL")
     else:
         retriever = get_retriever_in_website(url)
+        query = st.text_input("Ask a question to the website")
+        if query:
+            chain = (
+                {
+                    "docs": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
+            )
 
-        chain = {
-            "docs": retriever,
-            "question": RunnablePassthrough(),
-        } | RunnableLambda(get_answers)
-
-        chain.invoke("What is AlphaChip")
+            result = chain.invoke(query)
+            st.markdown(result.content)
