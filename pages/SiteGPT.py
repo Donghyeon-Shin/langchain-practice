a2f1e8abe5525f2138183162cb5a788020a08063
Donghyeon Shin <shindong0321@gmail.com>
2024-12-19 21:24:30 +0900

    Implement cache Embedding and map-rerank first chain

--- pages/SiteGPT.py
+++ pages/SiteGPT.py
@@ -1,9 +1,57 @@
 import asyncio
 import sys
+from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import SitemapLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't
+    Just say you don't know, don't make anyting up.
+
+    Then, give a score to the answer between 0 and 5. 0 being not helpful to
+    the user and 5 being helpful to the user.
+
+    Make sure to include the answer's score.
+
+    Context : {context}
+
+    Examples:
+
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+
+    Your turn!
+
+    Question : {question}
+    """
+)
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    answers = []
+    for doc in docs:
+        result = answers_chain.invoke({"context" : doc.page_content, "question" : question,})
+        answers.append(result.content)
+    st.write(answers)
+    return answers
 
 def parse_page(soup):
     header = soup.find("header")
@@ -15,18 +63,27 @@ def parse_page(soup):
     return str(soup.get_text()).replace("\n", " ").replace("\xa0", " ")
 
 
-@st.cache_data(show_spinner="Loading website....")
-def load_website(url):
+@st.cache_resource(show_spinner="Loading website....")
+def get_retriever_in_website(url):
     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1000,
         chunk_overlap=200,
     )
     loader = SitemapLoader(
-        url, filter_urls=[r"^(.*\/research\/).*"], parsing_function=parse_page
+        url,
+        parsing_function=parse_page,
     )
     loader.requests_per_second = 5
     docs = loader.load_and_split(text_splitter=splitter)
-    return docs
+    url_name = (
+        str(url).replace("https://", "").replace(".", "").replace("/sitemapxml", "")
+    )
+    st.write(url_name)
+    cache_dir = LocalFileStore(f"./.cache/Site_embeddings/{url_name}")
+    embedder = OpenAIEmbeddings()
+    cache_embedder = CacheBackedEmbeddings.from_bytes_store(embedder, cache_dir)
+    vector_store = FAISS.from_documents(docs, cache_embedder)
+    return vector_store.as_retriever()
 
 
 st.set_page_config(
@@ -56,5 +113,11 @@ if url:
         with st.sidebar:
             st.error("Please write down a Stiemap URL")
     else:
-        docs = load_website(url)
-        st.write(docs)
+        retriever = get_retriever_in_website(url)
+
+        chain = {
+            "docs": retriever,
+            "question": RunnablePassthrough(),
+        } | RunnableLambda(get_answers)
+
+        chain.invoke("What is AlphaChip")
