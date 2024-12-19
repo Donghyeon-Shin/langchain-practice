80a74a43257111fc298904b369bfef0db6cdf8dd
Donghyeon Shin <shindong0321@gmail.com>
2024-12-19 18:09:25 +0900

    Filter url and Customize html

--- pages/SiteGPT.py
+++ pages/SiteGPT.py
@@ -1,13 +1,31 @@
 import asyncio
 import sys
 from langchain.document_loaders import SitemapLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
+
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return str(soup.get_text()).replace("\n", " ").replace("\xa0", " ")
+
 
 @st.cache_data(show_spinner="Loading website....")
 def load_website(url):
-    loader = SitemapLoader(url)
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url, filter_urls=[r"^(.*\/research\/).*"], parsing_function=parse_page
+    )
     loader.requests_per_second = 5
-    docs = loader.load()
+    docs = loader.load_and_split(text_splitter=splitter)
     return docs
 
 
@@ -25,15 +43,7 @@ st.markdown(
     """
 )
 
-if "win32" in sys.platform:
-    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-    cmds = [["C:/Windows/system32/HOSTNAME.EXE"]]
-else:
-    cmds = [
-        ["du", "-sh", "/Users/fredrik/Desktop"],
-        ["du", "-sh", "/Users/fredrik"],
-        ["du", "-sh", "/Users/fredrik/Pictures"],
-    ]
+# https://deepmind.google/sitemap.xml
 
 with st.sidebar:
     url = st.text_input(
